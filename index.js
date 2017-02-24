7f755eb40629b6d865c329ee11b93493447a024c
Ben Thompson <misterpink14@gmail.com>
2017-02-24 07:01:55 -0700

    Done

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-console.log("hi");
+'use strict';
+
+const Hapi = require('hapi');
+const Bell = require('bell');
+const Config = require('./config');
+
+const server = new Hapi.Server();
+
+server.connection({ port: 3000, host: 'localhost' });
+
+
+server.register(Bell, (err) => {
+
+    if (err) {
+        throw err;
+    }
+
+    server.auth.strategy('facebook', 'bell', Config.facebook);
+
+    server.route({
+        method: "GET",
+        path: '/',
+        config: {
+            auth: 'facebook',
+            handler: (request, reply) => {
+                reply("Hello");
+            }
+        }
+    });
+
+    server.start((err) => {
+
+        if (err)
+            throw err;
+
+        console.log(`Server running at: ${server.info.uri}`);
+    });
+});
+
